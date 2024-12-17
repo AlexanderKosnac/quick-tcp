c7d33a95179330a90513970fbb4aba10176ab85a
Alexander Kosnac <alexander.kosnac@web.de>
2024-12-17 22:25:12 +0100

    Implement ClientView analogous to ServerView

---
+++ src/main/java/dev/ak/qtcp/view/ClientView.java
@@ -0,0 +1,49 @@
+package dev.ak.qtcp.view;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+public class ClientView extends JFrame {
+
+    private static int number = 1;
+
+    private static int pad = 5;
+
+    public ClientView() {
+        setTitle(String.format("Quick TCP Client #%d", number++));
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setSize(400, 300);
+
+        Container contentPane = getContentPane();
+        contentPane.setLayout(new GridBagLayout());
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(pad, pad, pad, pad);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+
+        // IP
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.weightx = 0.0;
+        contentPane.add(new JLabel("IP:"), gbc);
+
+        gbc.gridx = 1;
+        gbc.weightx = 1.0;
+        JTextField ipInput = new JTextField();
+        contentPane.add(ipInput, gbc);
+
+        // Port
+        gbc.gridx = 2;
+        gbc.weightx = 0.0;
+        contentPane.add(new JLabel("Port:"), gbc);
+
+        gbc.gridx = 3;
+        gbc.weightx = 1.0;
+        JTextField portInput = new JTextField();
+        contentPane.add(portInput, gbc);
+
+        setVisible(true);
+    }
+}
